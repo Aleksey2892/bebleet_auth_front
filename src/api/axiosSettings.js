b1061af872ecce2e18fa4a936fc29448cb3346ad
Oleksii Hryhorenko <aleksey2892@gmail.com>
2024-03-10 02:06:04 +0200

    fix link, move prettier to dev dependencies

--- src/api/axiosSettings.js
+++ src/api/axiosSettings.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const baseURL =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:3000'
-    : 'https://bebleet-auth-back.onrender.com';
+    : 'https://beebleet-auth-back.onrender.com';
 
 export const axiosInstance = axios.create({
   baseURL,
